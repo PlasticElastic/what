bcc1edd26f5889dd272eef3fd4115c220582585d
PlasticElastic <ucxakxah@gmail.com>
2019-09-10 00:45:19 +0300

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,3 +1,65 @@
+pipeline {
+    agent {
+        label "Linux_Default || masterLin"
+    }
+    options {
+        buildDiscarder(logRotator(
+                artifactDaysToKeepStr: '1',
+                artifactNumToKeepStr: '10',
+                daysToKeepStr: '1',
+                numToKeepStr: '10'))
+        skipDefaultCheckout()
+        ansiColor('xterm')
+        timeout(30)
+    }
+    parameters {
+        booleanParam name: 'haproxy', description: 'Развернуть конфиг', defaultValue: false
+        choice name: 'environment',
+                choices: ['',
+                          ''],
+                description: 'Среда развертывания'
+        choice name: 'type',
+                choices: ['lol',
+                          'wtf',],
+                description: 'Тип интеграции'
+        
+    }
+    stages {
+        stage('Download scripts') {
+            steps {
+                script {
+                    dir('pipelines') {
+                        checkout scm
+                    }
+                }
+            }
+        }
+        stage('Deploy applications') {
+            steps {
+                script {
+                    dir('pipelines/ansible') {
+                        def environment = ""
+                        switch (params.environment) {
+                            case "Приемочно-сдаточные испытания (ПСИ)":
+                                environment = "test"
+                                break
+                            case "Продакшн (ПРОД)":
+                                environment = "prod"
+                                break
+                        }
+                        def type = ""
+                        switch (params.type) {
+                            case "lol":
+                                type = "lol"
+                                break
+                            case "wtf":
+                                type = "wtf"
+                                break    
+                        }
+                        def hosts = "";
+                        if (params.haproxy) {
+                            hosts += "haproxy-" + type
+                        def tags = hosts
                                 {                                   
                                     ansiblePlaybook(
                                             installation: 'ansible26',
@@ -7,4 +69,13 @@
                                             extras: "--limit '${hosts}' -vvv",
                                             
                                     )    
-                        }   
+                        }                     
+
+                    }
+                }
+            }
+        }
+    }
+}
+}
+
